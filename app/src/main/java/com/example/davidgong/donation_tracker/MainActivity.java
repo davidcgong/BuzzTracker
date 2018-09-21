c2e83075372df36154dbdcd6d8d71d7ca5ad7945
David Gong <davidcgong@gmail.com>
2018-09-21 15:19:15 -0400

    Login button can navigate to login activity

--- app/src/main/java/com/example/davidgong/donation_tracker/MainActivity.java
+++ app/src/main/java/com/example/davidgong/donation_tracker/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.davidgong.donation_tracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button loginButton;
+    private Button registrationButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton = (Button) findViewById(R.id.logIn_button);
+        registrationButton = (Button) findViewById(R.id.registration_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
