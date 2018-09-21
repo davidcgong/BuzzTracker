bfe3092d66b61577a23cc40b975f66e163c1ca78
David Gong <davidcgong@gmail.com>
2018-09-21 15:43:13 -0400

    Fixed preview error

--- app/src/main/java/com/example/davidgong/donation_tracker/MainActivity.java
+++ app/src/main/java/com/example/davidgong/donation_tracker/MainActivity.java
@@ -26,5 +26,13 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        registrationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
