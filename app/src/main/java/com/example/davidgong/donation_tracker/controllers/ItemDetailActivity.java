c13d3fc77c0ea232bc035b66cf7f12a90f6804d8
Abhishek <abhishekmalle@gatech.edu>
2018-12-04 17:09:00 -0500

    Revert "Converted all controllers classes and LocationType to kotlin"
    This reverts commit 07bd56f34d53d2b181212382a53ca41b0184819e.

---
+++ app/src/main/java/com/example/davidgong/donation_tracker/controllers/ItemDetailActivity.java
@@ -0,0 +1,31 @@
+package com.example.davidgong.donation_tracker.controllers;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.davidgong.donation_tracker.R;
+import com.example.davidgong.donation_tracker.model.Item;
+
+public class ItemDetailActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_detail);
+
+        Item item = (Item) getIntent().getSerializableExtra("Item");
+
+        TextView info = findViewById(R.id.itemDetails);
+        info.setText(
+                "Added on " + item.getMonth() + "/" + item.getDay() + "/" + item.getYear() + " at " + item.getHour() + ":" + item.getMinute() + ".\n" +
+                        "Location: " + item.getLocation().getLocationName() + "\n" +
+                        "Description: " + item.getLongDesc() + "\n" +
+                        "Value: " + item.getValue() + "\n" +
+                        "Category: " + item.getItemType().name()
+        );
+
+
+    }
+
+}
